ba1320d81e806066580656a9d83f73b69d6d9b74
eatingTheNight <michaeljs1990@gmail.com>
2013-02-17 20:49:26 -0600

    second commit

---
+++ PyMiner/pyminer.py
@@ -0,0 +1,57 @@
+import optparse
+import urllib2
+import bs4
+import os.path
+
+def saveImgs(source):
+    filename = os.path.basename(source)
+    path = os.path.abspath(filename)
+    print "[*] Downloading " + filename
+    try:
+        url = urllib2.urlopen(source).read()
+    except Exception, e:
+        url = urllib2.urlopen('http:' + source).read()
+    fileDownload = open(path, 'w')
+    fileDownload.write(url)
+    fileDownload.close()
+    
+
+def getImgs(sourceCode):
+    soup = bs4.BeautifulSoup(sourceCode)
+    soup = soup.body
+    soup = soup.find_all('a', recursive=True)
+    for img in soup:
+        img = img.get('href')
+        try:
+            if any(ext in img for ext in ['.jpg', '.png', '.gif', '.jpeg']):
+                saveImgs(img)
+        except Exception, e:
+            print e
+
+
+def getPage(webpage):
+    #Grabs source code from page
+    url = urllib2.urlopen(webpage)
+    data = url.read()
+    url.close()
+    return data
+
+
+def main():
+    #Command line options parsed with optparse
+    usage = 'usage: %prog [options] arg'
+    parser = optparse.OptionParser(usage)
+    parser.add_option('-w', '--web', dest='webpage', help='webpage that you want data from')
+    parser.add_option('-i', '--img', dest='images', help='grabs all images from a webpage')
+    (options, args) = parser.parse_args()
+    if len((options,args)) != 2:
+        parser.error('Syntax: -w www.example.com --img [imgName]')
+    #more exception handeling to be implimented here
+    webpage = options.webpage
+    images = options.images
+    sourceCode = getPage(webpage)
+    getImgs(sourceCode)
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='PyMiner',
+    version='0.1dev',
+    packages=['PyMiner',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README').read(),
+)
