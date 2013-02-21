f47a74db320f876f1ed964cda832a52f1a5023eb
eatingTheNight <michaeljs1990@gmail.com>
2013-02-21 11:47:59 -0600

    5th commit

--- PyMiner/getImgs.py
+++ PyMiner/getImgs.py
@@ -6,10 +6,8 @@ import os
 def saveImgs(img, path):
     filename = os.path.basename(img)
     path = path + filename
-    if len(filename) > 10:
-        print "[*] Downloading " + filename[0:10]
-    else:
-        print "[*] Downloading " + filename
+    if os.path.exists(path):
+        return
     try:
         url = urllib2.urlopen(img).read()
     except Exception:
@@ -17,16 +15,24 @@ def saveImgs(img, path):
     fileDownload = open(path, 'w')
     fileDownload.write(url)
     fileDownload.close()
+    fileSize = os.path.getsize(path)
+    fileSize = fileSize / 1024
+    if len(filename) > 10:
+        print "[*] Downloading " + filename[0:10] + ' size: ' + str(fileSize) + 'kB'
+    else:
+        print "[*] Downloading " + filename + ' size: ' + str(fileSize) + 'kB'
     
 
 def getImgs(sourceCode):
     path = setPath.setPath()
     soup = bs4.BeautifulSoup(sourceCode)
     soup = soup.find_all('a', recursive=True, limit=None)
+    print soup
+    return
     for img in soup:
         img = img.get('href')
         try:
-            if any(ext in img for ext in ['.jpg', '.png', '.gif', '.jpeg']):
+            if any(ext in img for ext in ['.jpg', '.png', '.gif', '.jpeg']) and type(img) != None:
                 saveImgs(img, path)
         except Exception, e:
             print e
